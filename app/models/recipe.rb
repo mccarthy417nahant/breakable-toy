bf2fec83119db4cf0f12730f4e5b48c3f86cb566
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-12-29 16:33:46 -0500

    heroku

---
+++ app/models/recipe.rb
@@ -0,0 +1,18 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+
+  has_many :ingredients, inverse_of: :recipe, dependent: :destroy
+  has_many :directions, inverse_of: :recipe, dependent: :destroy
+
+  accepts_nested_attributes_for :ingredients,
+                                reject_if: proc { |attributes| attributes['name'].blank? },
+                                allow_destroy: true
+  accepts_nested_attributes_for :directions,
+                                reject_if: proc { |attributes| attributes['step'].blank? },
+                                allow_destroy: true
+
+  validates :title, :description, :image, presence: true
+
+  has_attached_file :image, styles: { :medium => "300x300" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
